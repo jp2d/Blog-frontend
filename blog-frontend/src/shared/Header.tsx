312d995f3187f75d8c3839aacebf9eb2a3a2037b
João Paulo Pimentel Dias <jp2d10@gmail.com>
2026-02-20 11:50:00 -0300

    Criação de cabeçalho

---
+++ blog-frontend/src/shared/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <div className="bg-light border-bottom py-3 text-center">
+      <h1 className="mb-0">Blogster</h1>
+      <p></p>
+      <small className="text-muted">Compartilhando ideias e conhecimento</small>
+    </div>
+  );
+};
+
+export default Header;
